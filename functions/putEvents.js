9650418c9f7be82e915a1fd7219a7ea943f9d66d
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-02-10 13:30:44 +0300

    test api gateway invocation

--- functions/putEvents.js
+++ functions/putEvents.js
@@ -1,4 +1,9 @@
 module.exports.handler = async (event) => {
     const body = JSON.parse(event.body);
     // put events to eventbridge
-}
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify("code is working")
+    };
+};
