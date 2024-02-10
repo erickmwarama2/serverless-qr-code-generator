1abea4936bee2088edd178dd0e9784a61787f38a
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-02-10 15:20:52 +0300

    log record body

--- functions/processEvents.js
+++ functions/processEvents.js
@@ -4,11 +4,12 @@ module.exports.handler = async (event) => {
 
     if (records.length > 0) {
         for (const record of records) {
+            console.log('record', record);
             try {
                 const parsedBody = JSON.parse(record.body);
                 console.log("processing vehicle details", record.body);
-                throw new Error('Error while processing');
-                // console.log("processing is succesfull", record.messageId);
+                // throw new Error('Error while processing');
+                console.log("processing is succesfull", record.messageId);
             } catch (error) {
                 batchItemFailures.push({
                     itemIdentifier: record.messageId
