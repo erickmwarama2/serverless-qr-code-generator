0fb48beb498914e790c12bda035962f759050eac
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-02-10 13:47:28 +0300

    publish events on to eventbridge

--- functions/putEvents.js
+++ functions/putEvents.js
@@ -1,9 +1,32 @@
+const EventBridge = require("aws-sdk/clients/eventbridge");
+const EVENT_BUS_NAME = process.env.eventBusName;
+
+let eventBridge = new EventBridge();
+
 module.exports.handler = async (event) => {
     const body = JSON.parse(event.body);
     // put events to eventbridge
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify("code is working")
+    let event = {
+        EventBusName: EVENT_BUS_NAME,
+        Detail: JSON.stringify({
+            vehicleNo: body.vehicleNo,
+            NIC: body.nic
+        }),
+        Source: "fuel-app",
+        DetailType: "user-signup"
     };
+
+    try {
+        let output = await eventBridge.putEvents({Entries: []}).promise();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(output)
+        };
+    } catch (error) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify(error.message)
+        };
+    }
 };
